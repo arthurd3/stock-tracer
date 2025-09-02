14408fa24405e1ea2fa4ddc4042fd317278428a4
arthurd3 <arthur_camposl@yahoo.com>
2025-09-02 10:45:05 -0300

    initiallize commit

---
+++ stock-tracer/src/main/java/com/arthur/stocktracer/StockTracerApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.stocktracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTracerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockTracerApplication.class, args);
+    }
+
+}
---
+++ stock-tracer/src/test/java/com/arthur/stocktracer/StockTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.stocktracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTracerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
